e604b2c264a79e3973fc229ee3dd86348be09873
Andriy Artamonov <andreiartamonovaa@gmail.com>
2021-11-24 17:25:31 +0200

    data_sets.html/new_schema - added html/css

--- schemas/urls.py
+++ schemas/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('schemas', views.data_schemas, name='schemas')
+    path('schemas', views.data_schemas, name='schemas'),
+    path('new-schema', views.new_schema, name='new_schema'),
+    path('data-sets', views.data_sets, name='data_sets'),
 ]
--- schemas/views.py
+++ schemas/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def data_schemas(request):
     return render(request, 'schemas/schemas.html')
+
+
+def new_schema(request):
+    return render(request, 'schemas/new_schema.html')
+
+
+def data_sets(request):
+    return render(request, 'schemas/data_sets.html')
